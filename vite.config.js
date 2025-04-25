776b27828e191dd55afcf614510dae1720109b04
xubeiyan <yueyongdong@gmail.com>
2025-04-25 10:50:39 +0800

    chore(vite.config.js): add deploy to certain site comment

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
+  // 在 http://chenhai.net/tools/subrip_subtitle_sort/ 上部署上需要调整 base
+  // base: '/tools/subrip_subtitle_sort/'
 });
